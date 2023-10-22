7f8b6e325d502857a29b9ac5cd6f5c10458d7ed7
Maxim <pushiniiist@gmail.com>
2023-10-22 20:20:27 +0200

    task 3 done

---
+++ lab6/task3.py
@@ -0,0 +1,21 @@
+fin = open("input3.txt", 'r', encoding="utf-8")
+
+min_age = 100
+max_age = 0
+
+
+
+
+for line in fin:
+    if int((line.rstrip("\n").split())[-1]) > max_age:
+        max_age = int((line.rstrip("\n").split())[-1])
+        foutmax = open("output3max.txt", 'w', encoding="utf-8")
+        foutmax.write(line)
+        foutmax.close()
+    elif int((line.rstrip("\n").split())[-1]) < min_age:
+        min_age = int((line.rstrip("\n").split())[-1])
+        foutmin = open("output3min.txt", 'w', encoding="utf-8")
+        foutmin.write(line)
+        foutmin.close()
+    else:
+        pass
