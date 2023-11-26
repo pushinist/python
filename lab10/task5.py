2906ad791f883273e869615e2a58fdb3ad0d2e12
Tsvetkov Maxim <pushiniiist@gmail.com>
2023-11-26 18:52:37 +0200

    task 5 done

--- lab10/task5.py
+++ lab10/task5.py
@@ -8,7 +8,7 @@ def calculate_mse(x, y):
 x = np.linspace(0, 1, 100)
 y = np.linspace(0, 1, 100)
 X, Y = np.meshgrid(x, y)
-Z = np.sin(2 * np.pi * X) * np.cos(2 * np.pi * Y)  # Пример функции
+Z = np.sin(2 * np.pi * X) * np.cos(2 * np.pi * Y) 
 
 mse_values = np.zeros_like(Z)
 for i in range(Z.shape[0]):
@@ -19,19 +19,19 @@ for i in range(Z.shape[0]):
 fig = plt.figure(figsize=(10, 5))
 
 ax1 = fig.add_subplot(121, projection='3d')
-ax1.plot_surface(X, Y, mse_values, cmap='viridis')
-ax1.set_title('MSE (обычный масштаб)')
+ax1.plot_surface(X, Y, mse_values)
+ax1.set_title('Среднеквадратичное отклонение')
 ax1.set_xlabel('X')
 ax1.set_ylabel('Y')
 ax1.set_zlabel('MSE')
 
 ax2 = fig.add_subplot(122, projection='3d')
-ax2.plot_surface(X, Y, mse_values, cmap='viridis')
-ax2.set_title('MSE (логарифмический масштаб)')
+ax2.plot_surface(X, Y, mse_values)
+ax2.set_title('Среднеквадратичное отклонение \n(с логарифмическим масштабом)')
 ax2.set_xlabel('X')
 ax2.set_ylabel('Y')
 ax2.set_zlabel('MSE')
-ax2.set_yscale('log')  
+ax2.set_zscale('log')   
 
 plt.tight_layout()
 plt.show()
